7bebf5b9ad8468557756a76fcadd4dcb6c0c930b
GE <GE@eztdeiMac.local>
2015-12-16 11:48:21 +0800

    Swift简单值

---
+++ Demo1/main.swift
@@ -0,0 +1,154 @@
+//
+//  main.swift
+//  Demo1
+//
+//  Created by GE on 15/12/15.
+//  Copyright © 2015年 GE. All rights reserved.
+//
+
+import Foundation
+
+//print("Hello, World!")
+//简单值
+    //
+    //let a:Float = 4
+    //
+    //print(a)
+    //
+    //let label = "The width is"
+    //let width = 94
+    //let widthLabel = label + " " + String(width)
+    //
+    //print(widthLabel)
+    //
+    //let m = 2.0
+    //let n = 3.0
+    //let words = "I give you \(m + n) apples"
+    //print(words)
+
+//控制流
+
+    //let individualScores = [75,43,103,87,12]
+    //var teamScore = 0
+    //
+    //for score in individualScores
+    //{
+    //    if score > 50
+    //    {
+    //        teamScore += 3
+    //    }else{
+    //        teamScore += 1
+    //    }
+    //}
+    //
+    //print(teamScore)
+
+    //var optionalString:String? = "Hello"
+    //print(optionalString == nil)
+    //
+    //var optionalName:String? = "John Appleseed"
+    //optionalName = nil
+    //var greeting = "Hello!"
+    //if let name = optionalName
+    //{
+    //    greeting = "Hello,\(name)"
+    //}
+    //print(greeting)
+    //
+    //let nickName:String? = nil
+    //let fullName:String = "John Appleseed"
+    ////另一种处理可选值的方法是通过使用 ?? 操作符来提供一个默认值。如果可选值缺失的话，可以使用默认值来代替。
+    //let informalGreeting = "Hi \(nickName ?? fullName)"
+    //
+    //print(informalGreeting)
+
+    //let vegetable = "red pepper"
+    //switch vegetable
+    //{
+    //    case "celery":
+    //        print("Add some raisins and make ants on a log.")
+    //    case "cucumber","watercress":
+    //        print("That would make a good tea sandwich")
+    //    case let x where x.hasSuffix("pepper"):
+    //        print("Is it a spicy \(x)?")
+    //    default:
+    //        print("Everything tastes good in soup.")
+    //}
+    //
+    //let interestingNumbers = [
+    //    "Prime":[2, 3, 5, 7, 11, 13],
+    //    "Fibonacci":[1, 1, 2, 3, 5, 8],
+    //    "Square":[1, 4, 9, 16, 25],
+    //]
+    //
+    //var largest = 0
+    //var larger = 0
+    //for (kind, numbers) in interestingNumbers
+    //{
+    //    for number in numbers
+    //    {
+    //        if number > largest
+    //        {
+    //            larger = largest
+    //            largest = number
+    //            
+    //        }
+    //    }
+    //}
+    //
+    //for (kind, numbers) in interestingNumbers
+    //{
+    //    for number in numbers
+    //    {
+    //        if number == largest
+    //        {
+    //            print("largest = \(kind)")
+    //        }
+    //        if number == larger
+    //        {
+    //            print("larger = \(kind)")
+    //        }
+    //    }
+    //}
+    //
+    //print(largest)
+    //print(larger)
+
+    //var n = 2
+    //while n < 100
+    //{
+    //    n = n * 2
+    //}
+    //print(n)
+    //
+    //var m = 2
+    //repeat
+    //{
+    //    m = m * 2
+    //}while m < 100
+    //
+    //print(m)
+
+    //var firstForLoop = 0
+    //
+    //for i in 0 ..< 4
+    //{
+    //    firstForLoop += 1
+    //}
+    //print(firstForLoop)
+
+    //var secondForLoop = 0
+    //
+    //for var i = 0; i < 4; i++
+    //{
+    //    secondForLoop += 1
+    //}
+    //print(secondForLoop)
+
+
+
+
+
+
+
+
